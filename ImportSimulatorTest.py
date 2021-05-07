f423e19035bba731819d2236d72ed6d31b2c6703
qu20523 <qu20523@bristol.ac.uk>
2021-05-07 21:46:57 +0100

    Minor Additions

--- ImportSimulatorTest.py
+++ ImportSimulatorTest.py
@@ -15,11 +15,50 @@ Simu = Simulator.Simulation(948, 10, 10, 10, 15, 5, 1, 1, 1000, 5, 7, 2, 1, 0.2,
 
 
 
-def Plotter(arraytoplot):
+
+
+class Plotter:
     
+    def __init__(self):
+        
+        self.fig = plt.figure(facecolor='w')
+        self.ax = self.fig.add_subplot(111, facecolor='#dddddd', axisbelow=True)
+        self.ax.set_xlabel('Time /days')
+        self.ax.set_ylabel('Number (1000s)')
+        self.ax.set_ylim(0,1.2)
+        self.ax.yaxis.set_tick_params(length=0)
+        self.ax.xaxis.set_tick_params(length=0)
+        self.ax.grid(b=True, which='major', c='w', lw=2, ls='-')
+        for spine in ('top', 'right', 'bottom', 'left'):
+            self.ax.spines[spine].set_visible(False)
+
+
+    def AddPlot(self, arraytoplot, colourcode, legendlabel):
+        
+        self.ax.plot(Simu.t, arraytoplot / 1000, colourcode, alpha=0.5, lw=2, label=legendlabel)
+        legend = self.ax.legend()
+        legend.get_frame().set_alpha(0.5)
+        
+        
+
+
+
+
+
+
+Plotter.AddPlot(Plotter(), Simu.S, 'b', 'Susceptible')
+Plotter.AddPlot(Plotter(), Simu.E, 'y', 'Exposed')
+
+
+#Plot(Simu.S, 'b', 'Suceptible' )
+
+'''
+
+
+def CreateGraph():
     fig = plt.figure(facecolor='w')
     ax = fig.add_subplot(111, facecolor='#dddddd', axisbelow=True)
-    ax.plot(Simu.t, arraytoplot / 1000, 'b', alpha=0.5, lw=2, label='Susceptible')
+#    ax.plot(Simu.t, arraytoplot / 1000, 'b', alpha=0.5, lw=2, label='Susceptible')
     ax.set_xlabel('Time /days')
     ax.set_ylabel('Number (1000s)')
     ax.set_ylim(0,1.2)
@@ -31,6 +70,12 @@ def Plotter(arraytoplot):
     for spine in ('top', 'right', 'bottom', 'left'):
         ax.spines[spine].set_visible(False)
     plt.show()
-    
 
-Plotter(Simu.S)
+def AddPlotts(arraytoplot, colourcode, label):
+    fig = plt.figure(facecolor='w')
+    ax = fig.add_subplot(111, facecolor='#dddddd', axisbelow=True)
+    ax.plot(Simu.t, arraytoplot / 1000, colourcode, alpha=0.5, lw=2, label=label)
+    plt.show
+    
+   '''
+
